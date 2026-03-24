3b2c8842474734ef006dce626c5b163bb9c59f5f
Raghav khandelwal <raghavkhandelwal483@gmail.com>
2026-03-25 02:29:56 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "./lib/utils";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +26,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className='light'
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={cn('min-h-screen font-sans antialiased grainy',geistSans.className)}>{children}</body>
     </html>
   );
 }
